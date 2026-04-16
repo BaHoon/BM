1b4b1fdd168a2445330aa5f80d73ce9958d5dbdf
LiuYihan <2352755@tongji.edu.cn>
2026-04-16 20:09:55 +0800

    chore: stop tracking data directory

--- data/app_foodyou/task_005_notice/ground_truth_src/app/src/commonMain/kotlin/com/maksimowiczm/foodyou/app/ui/notifications/NotificationsUiModule.kt
+++
@@ -1,9 +0,0 @@
-package com.maksimowiczm.foodyou.app.ui.notifications
-
-import com.maksimowiczm.foodyou.common.infrastructure.koin.userPreferencesRepository
-import org.koin.core.module.Module
-import org.koin.core.module.dsl.viewModel
-
-internal fun Module.notifications() {
-    viewModel { NotificationSettingsViewModel(userPreferencesRepository()) }
-}
--- data/app_newsreader/task_009/rssreader-main/app/src/main/java/tools/ReportBug.java
+++
@@ -1,9 +0,0 @@
-package tools;
-
-import android.content.Context;
-
-public final class ReportBug {
-    public static void enableMonitor(final Context ctx) {
-        //for future use
-    }
-}
